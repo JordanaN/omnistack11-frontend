90bb5e3129765c60c6018956402551f80b135deb
JordanaN <jordanasnogueira@gmail.com>
2020-03-31 00:24:38 -0300

    Adiciona Componentes de Login, Profile e Cadastro

---
+++ src/routes.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
+import Logon from './pages/Logon';
+import Register from './pages/Register';
+import Profile from './pages/Profile'
+
+export default function Routes() {
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route path="/" exact component={Logon} />
+        <Route path="/register" component={Register} />
+        <Route path="/profile" component={Profile} />
+      </Switch>
+    </BrowserRouter>
+  );
+}
+
+// Switch - usado para garantir que somente uma rota seja executada por vez
+// exact é usado para garantir que '/' funcione se não ele quebra as rotas de baixo pq p BrowserRouter nao entende o path corretamente
